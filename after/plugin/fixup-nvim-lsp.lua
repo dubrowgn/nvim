6afb19aa23904af9f9ecfbf7c32b048c4832f0a5
Dustin Brown <me@dubrowgn.com>
2025-01-11 09:12:22 +0100

    Work around 'vim' lua lsp warning

---
+++ after/plugin/fixup-nvim-lsp.lua
@@ -0,0 +1,17 @@
+local lspconfig = require('lspconfig')
+
+-- https://github.com/neovim/neovim/issues/21686
+lspconfig.lua_ls.setup {
+    settings = {
+        Lua = {
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+        },
+    },
+}
